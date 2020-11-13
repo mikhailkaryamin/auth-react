e3a4bdba43042df23b0fa5bffc9be3f595e3c714
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-11-13 17:48:24 +0500

    feat(App): create component

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Container from './Container';
+import Form from './Form';
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <Form />
+    </Container>
+  );
+};
+
+export default App;
