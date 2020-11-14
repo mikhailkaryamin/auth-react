bd1f59c07ad6e10e2a6c92fd03eb8b843d22af2b
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-11-14 20:28:13 +0500

    feat(Input): add function onSubmit(wrapper with preverntDefault) and prop onClick

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,15 +3,22 @@ import React from 'react';
 type Props = {
   placeholder?: string;
   prefix?: string;
+  onClick?: () => void;
   type: string;
   value?: string;
 }
 
-const Input: React.FC<Props> = ({ placeholder, prefix, type, value }: Props) => {
+const Input: React.FC<Props> = ({ placeholder, prefix, onClick, type, value }: Props) => {
+  const onSubmit = (evt: React.SyntheticEvent) => {
+    evt.preventDefault();
+    onClick?.();
+  };
+
   return (
     <input
       placeholder={placeholder}
       className={`input ${prefix ? prefix : ``}`}
+      onClick={onSubmit}
       type={type}
       value={value}
     />
