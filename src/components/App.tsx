ddffbde76c7430737a03f3978ee45d24d8f230b1
mikhailkaryamin <karyamin.mihail@yandex.ru>
2020-11-17 18:49:12 +0500

    feat(App): add routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
+import {
+  Switch,
+  Route,
+  RouteComponentProps
+} from 'react-router-dom';
+
+import { Route as AppRoute } from '../shared/routes';
 
 import Container from './Container';
-import Form from './Form';
+import Home from './Home';
+import FormAuth from './FormAuth';
 
 const App: React.FC = () => {
   return (
     <Container>
-      <Form />
+      <Switch>
+        <Route
+          exact
+          path={[AppRoute.HOME, AppRoute.ROOT]}
+          component={Home}
+        />
+        <Route
+          exact
+          path={AppRoute.LOGIN}
+          render={(props: RouteComponentProps) => {
+            return (
+              <FormAuth
+                route={props.location.pathname}
+              />
+            );
+          }}
+        />
+        <Route
+          exact
+          path={AppRoute.REGISTER}
+          render={(props: RouteComponentProps) => {
+            return (
+              <FormAuth
+                route={props.location.pathname}
+              />
+            );
+          }}
+        />
+      </Switch>
     </Container>
   );
 };
